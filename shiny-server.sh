eab45e8958cdf65098383b57f28af6639ab7daf9
Tong Chen <tche929@aucklanduni.ac.nz>
2019-10-28 11:42:08 +1300

    Update shiny-server.sh

--- shiny-server.sh
+++ shiny-server.sh
@@ -1,14 +1,7 @@
 #!/bin/sh
 
-# Make sure the directory for individual app logs exists
-mkdir -p /var/log/shiny-server
-chown shiny.shiny /var/log/shiny-server
+touch /var/log/shiny-server.log
+chown shiny:shiny /var/log/shiny-server.log
 
-if [ "$APPLICATION_LOGS_TO_STDOUT" != "false" ];
-then
-    # push the "real" application logs to stdout with xtail in detached mode
-    exec xtail /var/log/shiny-server/ &
-fi
-
-# start shiny server
-exec shiny-server 2>&1
+# run shiny server
+sudo -u shiny shiny-server >> /var/log/shiny-server.log 2>&1
