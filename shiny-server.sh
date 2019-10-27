9a2d61bf57852dbc6b7696fc58da4bc8c860fc39
Tong Chen <tche929@aucklanduni.ac.nz>
2019-10-28 12:55:11 +1300

    Update shiny-server.sh

--- shiny-server.sh
+++ shiny-server.sh
@@ -4,4 +4,4 @@ touch /var/log/shiny-server.log
 chown shiny:shiny /var/log/shiny-server.log
 
 # run shiny server
-sudo -u shiny shiny-server >> /var/log/shiny-server.log 2>&1
+exec sudo -u shiny shiny-server >> /var/log/shiny-server.log 2>&1
